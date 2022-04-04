d1e135cd2fc51e917f340686417d551c55b90668
MinyShrimp <ksk7584@gmail.com>
2022-04-05 02:09:10 +0900

    update

--- programmers/정수삼각형.py
+++ programmers/정수삼각형.py
@@ -4,40 +4,75 @@ def isOutOfRange( tri, col, row ):
         ( col < 0 or len(tri) <= col) or \
         ( row < 0 or len(tri[col]) <= row )
 
-_max = -1
 class Node:
-    def __init__(self, tri, value, col, row, root_sum):
-        global _max
+    def __init__(self, tri, value, col, row):
         self.value   = value
-        self.sum     = root_sum + value
-        _max = max( self.sum, _max )
-
-        self.left    = None if isOutOfRange(tri, col + 1, row)     else Node( tri, tri[col + 1][row],     col + 1, row,     self.sum ) # [col + 1, row]
-        self.right   = None if isOutOfRange(tri, col + 1, row + 1) else Node( tri, tri[col + 1][row + 1], col + 1, row + 1, self.sum ) # [col + 1, row + 1]
+        self.sum     = -1
+        self.p1      = None if isOutOfRange(tri, col - 1, row - 1) else [col - 1, row - 1]
+        self.p2      = None if isOutOfRange(tri, col - 1, row)     else [col - 1, row]
+        # self.isEnter = False
     
     def __str__(self) -> str:
-        return "value: {} / sum: {} / left: {} / right: {}".format(self.value, self.sum, self.left, self.right)
-    
-# def getTree(tri):
-#     return Node( tri, tri[0][0], 0, 0, 0 )
+        return "value: {}\tsum: {}\tp1: {}\tp2: {}".format(self.value, self.sum, self.p1, self.p2)
 
-# def DFT(tree, pos, sum):
-#     global _max
-#     node = tree[pos[0]][pos[1]]
+def setTree(tri):
+    for col_i, col_v in enumerate( tri ):
+        for row_i, row_v in enumerate( col_v ):
+            tri[col_i][row_i] = Node( tri, row_v, col_i, row_i )
+    tri[0][0].sum = tri[0][0].value
 
-#     sum += node.value
-#     if node.left == None or node.right == None:
-#         _max = max( _max, sum )
+def getNode( tri, pos ):
+    return None if pos == None else tri[pos[0]][pos[1]]
+
+def getSum( tri, node ):
+    if node.sum != -1:
+        return node.sum
+
+    p1 = getNode( tri, node.p1 )
+    p2 = getNode( tri, node.p2 )
+
+    if p1 == None and p2 != None:
+        if p2.sum != -1:
+            node.sum = node.value + p2.sum
+        else:
+            getSum( tri, p2 )
+            node.sum = node.value + p2.sum
+
+    elif p1 != None and p2 == None:
+        if p1.sum != -1:
+            node.sum = node.value + p1.sum
+        else:
+            getSum( tri, p1 )
+            node.sum = node.value + p1.sum
+
+    elif p1 != None and p2 != None:
+        if p1.sum == -1:
+            getSum( tri, p1 )
+            node.sum = node.value + p1.sum
+        if p2.sum == -1:
+            getSum( tri, p2 )
+            node.sum = node.value + p2.sum
+        if p1.sum != -1 and p2.sum != -1:
+            node.sum = node.value + max( p1.sum, p2.sum )
     
-#     if node.left != None:
-#         DFT( tree, node.left,  sum )
-#     if node.right != None:
-#         DFT( tree, node.right, sum )
-    
-def solution(triangle):
-    Node( triangle, triangle[0][0], 0, 0, 0 )
+    print(node, "\t|", p1, "\t|", p2)
+
+
+def search( tri ):
+    _max = -1
+    print("="*200)
+    for row in range( len(tri[-1]) ):
+        node = tri[-1][row]
+        getSum( tri, node )
+
+        _max = max( _max, node.sum )
+        print("="*200)
     
     return _max
 
-print( solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5], [9, 2, 7, 5, 4, 3]]) )
-# print( solution([[1], [2, 3], [4, 5, 6], [7, 8, 9, 10], [4, 5, 2, 6, 5]]) )
+def solution(triangle):
+    setTree(triangle)
+    return search(triangle)
+    
+# print( solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]) )
+print( solution([[1], [2, 3], [4, 5, 6], [7, 8, 9, 10], [4, 5, 2, 6, 5], [1,2,3,4,5,6]]) )
